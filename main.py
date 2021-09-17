38dfa0230665d0e48088910dce6e947225f3218a
ihak223 <73539358+ihak223@users.noreply.github.com>
2021-09-17 12:28:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 class Node:
   def __init__(self, data):
     self.data = data
-    self.source = None
-    self.links = []
+    self.source = None                             # Parent node.
+    self.links = []                                # Linked child node.
   def __repr__(self):
     return self.data
   def add(self, node):
@@ -15,19 +15,19 @@ class Tree:
     self.root = root
     self.nodes = [root]
   def add(self, node):
-    self.root.add(node)
+    self.root.add(node)                            # Adds node to root
   def get(self, coords):
     i = 0
     depth = len(coords)
     current_node = self.root
-    while i != depth:
-      current_node = current_node.links[coords[i]]
+    while i != depth:                              # Recurs until at correct depth.
+      current_node = current_node.links[coords[i]] # Adds node to links.
 
       i = i + 1
     return current_node
   def gadd(self, node, target_coords):
-    target = self.get(target_coords)
-    target.add(node)
+    target = self.get(target_coords)               # Gets requested node.
+    target.add(node)                               # Directly adds to target node.
     
     
 
